42cf26ba6f79381b78b3597afc9ad900fb85e201
Scott <skenning128@gmail.com>
2023-03-02 16:35:20 -0800

    finnhub api working

--- website/src/components/StockData.jsx
+++ website/src/components/StockData.jsx
@@ -1,29 +1,36 @@
 import { useEffect, useState } from "react"
-import {DefaultApi, ApiClient} from "finnhub";
+import { ApiClient, DefaultApi } from "finnhub/dist";
 
 
 
 export const StockData = (props) => {
-    const api_key = ApiClient.instance.authentications['api_key'];
-    api_key.apiKey = "cfpv98pr01qmi6j4cj90cfpv98pr01qmi6j4cj9g"
-    const finnhubClient = new DefaultApi()
-    
-    // Stock candles
-    finnhubClient.stockCandles("AAPL", "D", 1590988249, 1591852249, (error, data, response) => {
-        console.log(data)
-    });
-
+    const finnhub = require('finnhub');
     const [data, setData] = useState();
     const [loading, setLoading] = useState(true);
+    // const api_key = ApiClient.instance.authentications['api_key'];
+    // api_key.apiKey = "cfpv98pr01qmi6j4cj90cfpv98pr01qmi6j4cj9g"
+    // const finnhubClient = new DefaultApi()
+    
+    // // Stock candles
+    
+
 
     const fetchData = async () => {
         try {
-            const data = await (
-                await fetch(
-                  `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${props.symbol}&interval=5min&apikey=GT2HOU06URGEHO0T`
-                )
-              ).json();
-            setData(data)
+            // const data = await (
+            //     await fetch(
+            //       `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${props.symbol}&interval=5min&apikey=GT2HOU06URGEHO0T`
+            //     )
+            //   ).json();
+            // setData(data)
+
+            const api_key = finnhub.ApiClient.instance.authentications['api_key'];
+            api_key.apiKey = "cfpv98pr01qmi6j4cj90cfpv98pr01qmi6j4cj9g"
+            const finnhubClient = new finnhub.DefaultApi()
+
+            finnhubClient.stockCandles("AAPL", "D", 1590988249, 1591852249, (error, data, response) => {
+                setData(data)
+            });
             setLoading(false);
         } catch (err) {
             alert(err);
