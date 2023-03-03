92fa9e8374d737dc2b2096289809db6da247edd2
Christian Bookout <christianmbookout@gmail.com>
2023-03-02 16:37:12 -0800

    add very very basic docker bash and bat scripts

--- website/build.dockers.bat
+++ website/build.dockers.bat
@@ -1,9 +1,9 @@
-#!/bin/bash
+@echo off
 
-# building flask docker image and running it
+@REM building flask docker image and running it
 docker image build -t flask -f Dockerfile.flask .
 docker run -p 5000:5000 -d flask
 
-# building node docker image and running it
+@REM building node docker image and running it
 docker image build -t node -f Dockerfile.node .
 docker run -p 3000:3000 node
