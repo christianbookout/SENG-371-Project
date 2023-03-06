269ae057928e740ae36270b00a3683e6d56c7373
Scott <skenning128@gmail.com>
2023-03-06 00:10:20 -0800

    merge main, fix import issue

--- website/src/components/NewsData.jsx
+++ website/src/components/NewsData.jsx
@@ -1,45 +1,49 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 // TODO make this work with the API instead of the JSON file
-import jsonData from "../../api/articles.json"
-import { NewsArticle } from "./NewsArticle"
+import jsonData from "../articles.json";
+import { NewsArticle } from "./NewsArticle";
 
 //Cuts off the title of articles that are over 90 characters to prevent cut-offs, then appends "..." to them.
-function title_format(title){
-    var string1 = JSON.stringify(title).replace(/['"]+/g, '')
-    if(string1.length > 90){ 
-        string1 = string1.slice(0, 90) + "..."
-    }
-    return string1
+function title_format(title) {
+  var string1 = JSON.stringify(title).replace(/['"]+/g, "");
+  if (string1.length > 90) {
+    string1 = string1.slice(0, 90) + "...";
+  }
+  return string1;
 }
-
 
 export const NewsData = (props) => {
-    const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(true);
 
-    const fetchData = () => {
-            setLoading(false);
-    }
+  const fetchData = () => {
+    setLoading(false);
+  };
 
-    useEffect(() => {
-        fetchData();
-    }, []);
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    let arr = [];
-    while(arr.length < props.articles){
-        var r = Math.floor(Math.random() * 290) + 1;
-        if(arr.indexOf(r) === -1) arr.push(r);
-    }
-    console.log(arr);
+  let arr = [];
+  while (arr.length < props.articles) {
+    var r = Math.floor(Math.random() * 290) + 1;
+    if (arr.indexOf(r) === -1) arr.push(r);
+  }
+  console.log(arr);
 
-
-    return (
-            <div className="flex flex-wrap gap-4 w-full h-full justify-between">
-                { loading ? "Loading..." : 
-                    arr.map(num => {
-                        const article = jsonData.articles[num]
-                        return <NewsArticle url = {article.url} urlToImage={article.urlToImage} title={title_format(article.title)}/>
-                    })
-                }
-            </div>
-    )
-}
+  return (
+    <div className="flex h-full w-full flex-wrap justify-between gap-4">
+      {loading
+        ? "Loading..."
+        : arr.map((num) => {
+            const article = jsonData.articles[num];
+            return (
+              <NewsArticle
+                url={article.url}
+                urlToImage={article.urlToImage}
+                title={title_format(article.title)}
+              />
+            );
+          })}
+    </div>
+  );
+};
