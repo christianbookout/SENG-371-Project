e63a95aa99a3bf16ef56c030790ce5cf4f8c02d3
Scott <skenning128@gmail.com>
2023-03-15 12:45:27 -0700

    sell container formatting

--- website/src/components/Sell/Sell.jsx
+++ website/src/components/Sell/Sell.jsx
@@ -11,7 +11,7 @@ export const Sell = () => {
       <div className="flex w-full flex-col">
         <SearchBar value={search} onChange={setSearch} />
         <h2 className="mt-8 text-xl">Your stocks:</h2>
-        <div className="flex h-full max-h-96 max-h-full flex-col divide-y-2 overflow-hidden overflow-y-scroll rounded border-gray-600 bg-white shadow">
+        <div className="flex h-full max-h-[21.5em] max-h-full flex-col divide-y-2 overflow-y-scroll rounded border-gray-600 bg-white shadow">
           <SellCard />
           <SellCard />
           <SellCard />
