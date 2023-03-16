1511233963ad0bf8034c838814541dcf76ef9165
tdorman <trevordorman7@gmail.com>
2023-03-16 16:03:19 -0700

    testing yaml credential pull

--- website/api/api.py
+++ website/api/api.py
@@ -1,8 +1,20 @@
 import time
 from flask import Flask
+import yaml
+import mysql.connector
+import json
 
 app = Flask(__name__)
-
+##Configure db
+# Load database credentials from db.yaml
+db = yaml.load(open('db.yaml'), Loader=yaml.FullLoader)
+# Connect to the database
+db = mysql.connector.connect(
+    host=db['mysql_host'], 
+    user=db['mysql_user'], 
+    password=db['mysql_password'], 
+    database=db['mysql_db']
+    )
 @app.route('/time')
 def get_current_time():
     return {'time': 10}
