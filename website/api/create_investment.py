d9d6c944bcf6b200353b824d78599a25572bfe39
tdorman <trevordorman7@gmail.com>
2023-03-22 23:19:49 -0700

    added new API routes for users and investments

--- website/api/create_investment.py
+++ website/api/create_investment.py
@@ -16,16 +16,46 @@ def create_investment(request):
     purchased_at = datetime.now()
     purchased_price = request.json['purchased_price']
     quantity = request.json['quantity']
-    query = "INSERT INTO Investments (owner, ticker, purchased_at, purchase_price, quantity) VALUES (%s, %s, %s, %s, %s)", 
-    args = (owner, ticker, purchased_at, purchased_price, quantity)
+    query = "INSERT INTO Investments (owner, ticker, quantity) VALUES (%s, %s, %s);", 
+    args = (owner, ticker, quantity)
+    send_query(db, query, args) 
+    query = "INSERT INTO Transactions (purchaser, ticker, price, time, buy_sell) VALUES (%s, %s, %s, %s, %s, 'buy');"
+    args = (owner, ticker, purchased_price, purchased_at)
     send_query(db, query, args)
     return "", 200
 
 @app.route('/updateStock', methods=['PATCH'])
-def update_investments(quantity, ticker):
+def update_investments(request):
     """Updates the quantity of an investment in the database"""
+    
+    quantity = request.json['quantity']
+    ticker = request.json['ticker']
     query = "UPDATE Investments SET quantity = %s WHERE ticker = %s"
     args = (quantity, ticker)
-    result = send_query(db, query, args)
-    return result
+    send_query(db, query, args)
+    return "", 200
     
+@app.route('/sellStock', methods=['POST'])
+def sell_investment(request):
+    """Creates a new investment entry in the database"""
+
+    ticker = request.json['ticker']
+    owner = request.json['owner']
+    sold_at = datetime.now()
+    sold_price = request.json['sold_price']
+    quantity = request.json['quantity']
+    query = "INSERT INTO Transactions (purchaser, ticker, price, time, buy_sell) VALUES (%s, %s, %s, %s, 'sell');"
+    args = (owner, ticker, sold_price, sold_at)
+    send_query(db, query, args)
+    query = "SELECT quantity FROM Investments WHERE ticker = %s AND owner = %s;"
+    args = (ticker, owner)
+    result = send_query(db, query, args)
+    if (result[0][0] == quantity):
+        query = "DELETE FROM Investments WHERE ticker = %s AND owner = %s;"
+        args = (ticker, owner)
+        send_query(db, query, args)
+    else:
+        query = "UPDATE Investments SET quantity = %s WHERE ticker = %s AND owner = %s;"
+        args = (result[0][0] - quantity, ticker, owner)
+        send_query(db, query, args)
+    return "", 200
--- website/api/create_user.py
+++ website/api/create_user.py
@@ -3,17 +3,20 @@ import mysql.connector
 import json
 import time
 from utils import *
+from api import db, app
 
 
-
+@app.route('/createUser', methods=['POST'])
 def create_user(request):
     """Creates a new user entry in the database"""
 
     name = request.json['fullname']
     email = request.json['email']
     password = request.json['password']
-    query = f"INSERT INTO Users(fullname, email, password, balance) VALUES ('{name}', '{email}', '{password}', 25000);"
-    result = send_query(query)
+    date = date.today()
+    query = "INSERT INTO Users(fullname, email, password, balance, created_at) VALUES (%s, %s, %s 25000, %s);"
+    args = (name, email, password, date)
+    result = send_query(db, query, args)
     return result
 
  
