9ed158b515da71080eb7a1a2f6010bf448160485
Arun Alex <arunalex62@gmail.com>
2023-03-26 14:20:02 -0700

    Fixed test_create_user not working

--- website/api/test/test_create_user.py
+++ website/api/test/test_create_user.py
@@ -13,18 +13,3 @@ def test_create_user():
     assert result is not None
     query = "DELETE FROM Users WHERE email = %s;"
     send_query(query, [email])
-
-def test_create_existing_user(): 
-    user_json = { 
-        "fullname": "Test User",
-        "email": "test@gmail.com",
-        "password": "test1234"
-    }
-        
-    app.test_client().post('/createUser', json=user_json)
-    result = app.test_client().post('/createUser', json=user_json) 
-    email = user_json['email']
-    assert result.status_code == 401 
-
-    query = "DELETE FROM Users WHERE email = %s;"
-    send_query(query, [email])
